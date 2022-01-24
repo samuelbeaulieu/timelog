794edda054b227ef7487b19e5d3123c695f28131
Samuel Beaulieu <samuel.beaulieu28@gmail.com>
2022-01-23 21:01:56 -0500

    Simplified week picker

--- timelog/ContentView.swift
+++ timelog/ContentView.swift
@@ -35,61 +35,6 @@ struct SheetView: View {
     
     @State private var timeCount = "0h 0m"
     
-    let weeks = [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6,
-        7,
-        8,
-        9,
-        10,
-        11,
-        12,
-        13,
-        14,
-        15,
-        16,
-        17,
-        18,
-        19,
-        20,
-        21,
-        22,
-        23,
-        24,
-        25,
-        26,
-        27,
-        28,
-        29,
-        30,
-        31,
-        32,
-        33,
-        34,
-        35,
-        36,
-        37,
-        38,
-        39,
-        40,
-        41,
-        42,
-        43,
-        44,
-        45,
-        46,
-        47,
-        48,
-        49,
-        50,
-        51,
-        52
-    ]
-    
     var body: some View {
         Form {
             Section() {
@@ -107,7 +52,7 @@ struct SheetView: View {
                 Text("Ends")
             }
             Picker("Week of Year", selection: $currentWeek) {
-                ForEach(weeks, id: \.self) { week in
+                ForEach(1...52, id: \.self) { week in
                     Text("Week \(week)")
                 }
             }
